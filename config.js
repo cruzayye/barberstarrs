830848599ddaf736bd23c6ac274180ecbed8c86f
cruzayye <johnnyc010392@gmail.com>
2019-05-03 19:53:36 -0700

    set hover for names

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
+const mapKey = 'AIzaSyBe48Rq5YpegRwCh4OvVTXz8y-jPHB_GCs';
 
 
-module.epxorts(() => {
-  return '
-})
+module.exports = { mapKey }; 
--- script.js
+++ script.js
@@ -5,11 +5,27 @@
 //button scroll
 
 $(document).on('click', 'a[href^="#"]', function(event){
-	event.preventDefault();
+  event.preventDefault();
 	
-	$('html, body').animate({
-		scrollTop: $($.attr(this,'href')).offset().top
-	
-	
-	});
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+
+  });
 });
+
+// $('.barbers').on('hover', 'div', function() {
+//   $(this).addClass('barberDisplay');
+// });
+
+
+$('.barbers').hover(
+  function() {
+    $(this).addClass('barberDisplay').fadeIn(2000);
+  }, function() {
+    $(this).removeClass('barberDisplay');
+  }
+);
+
+
+
+
