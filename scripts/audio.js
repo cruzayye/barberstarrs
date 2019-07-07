94bfbfda235402c476bbd1982d3dcf666a18c172
cruzayye <johnnyc010392@gmail.com>
2019-07-06 17:47:34 -0700

    play pause functionality in effect

---
+++ scripts/audio.js
@@ -0,0 +1,40 @@
+
+const x = document.getElementById('audio');
+
+$(document).ready(function() {
+  var btn = $(".button");
+  btn.click(function() {
+    btn.toggleClass("paused");
+    return false;
+  });
+});
+
+
+function playAudio() {
+  x.play();
+  
+}
+
+
+// window.onload = function() {
+//   x.play();
+// };
+
+$('#play').on('click', function() {
+  x.pause();
+});
+
+
+
+
+
+
+
+// $(document).on('click', 'a[href^="#"]', function(event){
+//   event.preventDefault();
+	
+//   $('html, body').animate({
+//     scrollTop: $($.attr(this, 'href')).offset().top
+
+//   });
+// });
