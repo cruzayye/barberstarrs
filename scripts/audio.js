a2285808d2631674c05a42acb8172ed47832ec13
cruzayye <johnnyc010392@gmail.com>
2019-07-26 14:03:45 -0700

    changed class name

--- scripts/audio.js
+++ scripts/audio.js
@@ -2,7 +2,7 @@
 const x = document.getElementById('audio');
 
 $(document).ready(function() {
-  var btn = $('.button');
+  var btn = $('.audio-button');
   btn.click(function() {
     btn.toggleClass('paused');
     return false;
@@ -14,7 +14,7 @@ jQuery.fn.clickToggle = function(a, b) {
   return this.on('click', function(ev) { [b, a][this.$_io ^= 1].call(this, ev); });
 };
 
-$('.button').clickToggle(function(ev) {
+$('.audio-button').clickToggle(function(ev) {
   x.pause();
 }, function(ev) {
   x.play();
