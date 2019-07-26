080837bc0ca2d65b4e42cf87f308b5ac33d285b7
cruzayye <johnnyc010392@gmail.com>
2019-07-26 13:45:37 -0700

    play button works

--- scripts/audio.js
+++ scripts/audio.js
@@ -1,6 +1,4 @@
 
-
-
 const x = document.getElementById('audio');
 
 $(document).ready(function() {
@@ -11,12 +9,7 @@ $(document).ready(function() {
   });
 });
 
-
-
-window.onload = function() {
-  document.getElementById('audio').play();
-};
-
+//Play button
 jQuery.fn.clickToggle = function(a, b) {
   return this.on('click', function(ev) { [b, a][this.$_io ^= 1].call(this, ev); });
 };
@@ -27,42 +20,6 @@ $('.button').clickToggle(function(ev) {
   x.play();
 });
 
-
-
-$('.button').on('click', 'toggle', function() {
-  alert('First handler for .toggle() called.');
-}, function() {
-  alert('Second handler for .toggle() called.');
-});
-
-//  $('#button').toggle(function() {
-//   x.pause();
-// }, function() {
-//   x.play();
-// });
-
-
-
-
-// function play_audio(task) {
-//   if(task == 'play'){
-//     $('.my_audio').trigger('play');
-//   }
-//   if(task == 'stop'){
-//     $('.my_audio').trigger('pause');
-//     $('.my_audio').prop('currentTime', 0);
-//   }
-// }
-
-
-
-
-
-// $(document).on('click', 'a[href^="#"]', function(event){
-//   event.preventDefault();
-	
-//   $('html, body').animate({
-//     scrollTop: $($.attr(this, 'href')).offset().top
-
-//   });
-// });
+window.onload = function() {
+  document.getElementById('audio').play();
+};
