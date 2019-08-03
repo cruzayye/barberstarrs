2c9134a42c71bc1a62e23418d04b096555f13698
cruzayye <johnnyc010392@gmail.com>
2019-08-02 22:18:19 -0700

    modal works

--- scripts/musicModal.js
+++ scripts/musicModal.js
@@ -3,6 +3,10 @@ const span = document.getElementsByClassName('close')[0];
 
 const modal = document.getElementById('music-modal');
 
+const yes = document.getElementById('yes-btn');
+
+const no = document.getElementById('no-btn');
+
 const hideModal = () => {
   modal.style.display = 'none';
 };
@@ -12,8 +16,10 @@ const playMusic = () => {
 };
 
 window.onclick = function(event) {
-  if(event.target === modal) {
+  if(event.target === modal || event.target === yes) {
     playMusic();
+    hideModal();
+  } else if(event.target === no) {
     hideModal();
   }
 };
